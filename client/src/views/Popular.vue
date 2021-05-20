482bed575bd0e78a71fc64a8ea1294d700ef5040
ssw02238 <ssw0223820@gmail.com>
2021-05-20 12:36:43 +0900

    popular + movielist

--- client/src/views/Popular.vue
+++ client/src/views/Popular.vue
@@ -1,31 +1,36 @@
 <template>
-  <div>
-    <button @click="movieList">get popular</button>
-
+  <div class="container">
+    <div class="row">
+    <MovieLists
+    v-for="(pop_movie, idx) in popular" :key="idx"
+    :pop_movie="pop_movie"/>
+  </div>
   </div>
 </template>
 
 <script>
+import MovieLists from "@/components/MovieLists"
 import { movieApi } from "../utils/axios"
-import request from "../utils/axios"
 
 export default {
-  name: 'popular',
+  name: 'Popular',
+  components: {
+    MovieLists
+  },
   data: function () {
     return {
       popular: {},
-      url: {},
     }
   },
-  methods: {
-    movieList: function () {
-      this.popular = movieApi.popular
-      this.url = request
-      console.log(movieApi.popular)
-      console.log(movieApi)
-
+  async mounted() {
+  try {
+    const { data } = await movieApi.popular()
+    console.log(data.results)
+    this.popular = data.results
+    } catch (error) {
+      console.log(error)
     }
-  }
+  }, 
 }
 </script>
 
