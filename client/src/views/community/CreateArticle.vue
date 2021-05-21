43c611642433de64e763658d95de569eec5b8487
kt5965 <kth5965@gmail.com>
2021-05-21 21:00:08 +0900

    123123

--- client/src/views/community/CreateArticle.vue
+++ client/src/views/community/CreateArticle.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="mb-5">
       <span>별점: </span>
-      <input placeholder="숫자" type="number" min='0' max='5' v-model.trim="rating" @keyup.enter="createArticle"> <br> 
+      <input placeholder="숫자를 입력하세요" type="text" v-model.trim="rating" @keyup.enter="createArticle"> <br> 
     </div>
     <button @click="createArticle" class="btn btn-danger mb-3">작성하기</button>
   </div>
@@ -26,7 +26,6 @@ export default {
       title: null,
       content: null,
       rating: null,
-      movies: [],
     }
   },
   methods: {
@@ -60,26 +59,6 @@ export default {
           })
         }
     },
-    getMovies: function () {
-      axios({
-        method: 'get',
-        url: 'http://127.0.0.1:8000/movies/movieList',
-        headers: this.setToken()
-      })
-        .then((res) => {
-          this.movies = res.data
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-    }
-  },
-    created: function () {
-      if (localStorage.getItem('jwt')) {
-        this.getMovies()
-      } else {
-        this.$router.push({name: 'Login'})
-      }
   }
 }
 </script>
