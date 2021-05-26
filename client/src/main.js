4ef4823b7b4712e5e96d0711fad9cb6082ef82c9
kt5965 <kth5965@gmail.com>
2021-05-26 22:30:46 +0900

    final

--- client/src/main.js
+++ client/src/main.js
@@ -5,10 +5,11 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(BootstrapVue)
-
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
